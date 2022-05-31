a6d0a7d223841dc0d2981f487784f0bc32fb2ba7
MareaNorto <nortonmarea@gmail.com>
2022-05-31 14:46:40 +0930

    finish #13 usingfunctions as props

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,16 @@ const Home = () => {
       { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
       { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id)
+      setBlogs(newBlogs);
+    }
   
    return (
       <div className="home">
-         <BlogList blogs={blogs} title="All Blogs!" />
-         <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Marios' blogs" />
-      </div>
+         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      </div>   
    );
 }
  
